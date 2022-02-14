e8ba935b6e3ce8776b539dde4877a0c497db55b4
Marek Szymczak <marek.szymczak04@gmail.com>
2022-02-14 22:47:11 +0100

    change main

--- XILINX_BASYS_3.srcs/sources_1/new/main.v
+++ XILINX_BASYS_3.srcs/sources_1/new/main.v
@@ -21,28 +21,81 @@
 
 
 module main(
+    //////////// CLOCK //////////
     input           CLK_100MHz,
-    output  [15:0]  led,
-    output  [3:0]   an,
-    output  [3:0]   an,
-    output          dp,
+    
+    //////////// SWITCHES //////////
+    input   [15:0]  SW,
+    
+    //////////// BUTTONS //////////  
     input           btnC,
     input           btnU,
     input           btnL,
     input           btnR,
     input           btnD,
-    input           reset,
-    output  [6:0]   seg,
-    input           usb_uart_rxd,
-    output          usb_uart_txd,
-    input           vauxp15  
+    
+    //////////// LEDS //////////
+    output  [15:0]  LED,
+    
+    //////////// UART //////////
+    input           UART_RX,
+    output          UART_TX,   
+    
+    //////////// 7-seg Display //////////
+    output  [6:0]   SEG,
+    output  [3:0]   AN,
+    output          DP
+
 );
 
+	//	Clocks
+	wire 				clk_10MHz;	
+	wire 				clk_1Hz;	
+	
+	// Buttons
+	wire 				rst;
+	wire 				n_rst;
 
 
 
+//===================================================================                       
+//                         Clocks
+//===================================================================
+	clk_div    clk_div_0	(CLK_100MHz, 0, clk_1Hz);
+	           clk_div_0	(clk_10MHz, 0, clk_10Hz);
+			   defparam 	clk_div_0.div_by	= 100_000_000;
+			   defparam 	clk_div_0.div_by	= 1_000_000;
+
+    clk_wiz_1 clk_wiz_1_0(
+        // Clock out ports
+        .clk_out1(clk_10MHz),     // output clk_out1
+        // Status and control signals
+        .reset(rst), // input reset
+        .locked(),       // output locked
+       // Clock in ports
+        .clk_in1(CLK_100MHz)
+    );      
 
 
+//===================================================================                       
+//                       Processor CPU1
+//===================================================================
+CPU_1   CPU_1_i(
+        .IO_in1     ({28'd0, btnU, btnL, btnR, btnD}),
+        .IO_in2     (0),
+        .IO_out1    ({31'd0, LED[2]}),
+        .IO_out2    (IO_out2),
+        .UART_RX1   (UART_RX),
+        .UART_TX1   (UART_TX),
+        .clk_in1    (CLK_100MHz),
+        .reset      (btnC)
+);
+
+assign rst      = btnC;
+assign LED[15]  = btnC;
+assign LED[0]   = clk_1Hz;
+assign LED[1]   = clk_10Hz;
+/*
   BASYS3 BASYS3_i
        (.an(an),
         .dp(dp),
@@ -54,16 +107,6 @@ module main(
         .usb_uart_rxd(usb_uart_rxd),
         .usb_uart_txd(usb_uart_txd),
         .vauxp15(vauxp15));
-
-
-
-
-
-    
- 
-	
-	assign led[5] = RST;
-
-
+*/
 
 endmodule
